b5f059d0f581d04896733389039e3e123474a330
dhanyafaith123 <dhanya.krishnans@faithinfotech.in>
2024-01-31 17:30:10 +0530

    testing Gittestdemo

---
+++ GitTestDemo/Controllers/TestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GitTestDemo.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
